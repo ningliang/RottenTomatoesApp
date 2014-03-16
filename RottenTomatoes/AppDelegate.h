e03c5d3bb615ea0ff02ca36cc7a6a5eaedd76df8
Ning Liang <ning.q.liang@gmail.com>
2014-03-16 16:37:02 -0700

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Ning Liang on 3/16/14.
+//  Copyright (c) 2014 Ning Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
